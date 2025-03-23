1257986bb629f7dbca6617edc24025f7460afad7
hoangbaoaz <11220793@st.neu.edu.vn>
2025-03-23 14:52:47 +0700

    tao cay danh muc

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -2,26 +2,41 @@ const ProductCategory = require("../../models/product-category.model");
 
 const systemConfig = require("../../config/system");
 
+const createTreeHelper = require("../../helpers/createTree")
+
 // [GET] /admin/product-category
 module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
   };
 
+
   const records = await ProductCategory.find(find);
+
+  const newRecords = createTreeHelper.tree(records);
 
   res.render("admin/pages/product-category/index", {
     pageTitle: "Danh muc san pham",
-    records: records,
+    records: newRecords,
   });
 };
+//[GET] /admin/product-category/create
+module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
 
-module.exports.create = (req, res) => {
+  const records = await ProductCategory.find(find);
+
+  const newRecords = createTreeHelper.tree(records);
+
   res.render("admin/pages/product-category/create", {
     pageTitle: "Tao danh muc san pham",
+    records: newRecords,
   });
 };
 
+//[POST] /admin/product-category/create
 module.exports.createPost = async (req, res) => {
   if (req.body.position == "") {
     const count = await ProductCategory.countDocuments();
