2a8e647a29c9298c387d075ea541046254ee36e0
hoangbaoaz <11220793@st.neu.edu.vn>
2025-03-26 20:34:56 +0700

    san pham noi bat

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,20 @@
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+  //Lay ra san pham noi bat
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active",
+  }).limit(6);
+  //Het lay ra san pham noi bat
+
+  const newProducts = productsHelper.priceNewProduct(productsFeatured);
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chu",
+    productsFeatured: newProducts,
   });
 };
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -7,12 +8,7 @@ module.exports.index = async (req, res) => {
     deleted: false,
   }).sort({ position: "desc" });
 
-  const newProducts = products.map((item) => {
-    item.priceNew = (item.price * (1 - item.discountPercentage / 100)).toFixed(
-      2
-    );
-    return item;
-  });
+  const newProducts = productsHelper.priceNewProduct(products);
 
   res.render("client/pages/products/index", {
     pageTitle: "Trang danh sach san pham",
